65d9921e6e4d41bfab720d46bc1910113103efba
Maxi <maxinunez2455@gmail.com>
2025-11-07 00:33:59 -0300

    Creación de navegador

--- app/src/main/java/com/maxi/cfp401/cursoappsmoviles/navegacion/Navegador.kt
+++ app/src/main/java/com/maxi/cfp401/cursoappsmoviles/navegacion/Navegador.kt
@@ -1,10 +1,13 @@
 package com.maxi.cfp401.cursoappsmoviles.navegacion
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.maxi.cfp401.cursoappsmoviles.ajedrez.AjedrezApp
 
+@Preview(showSystemUi = true)
 @Composable
 fun Navegador(){
     val navController = rememberNavController()
@@ -13,7 +16,10 @@ fun Navegador(){
         "launcher_screen"
     ){
         composable("launcher_screen"){
-            LauncherScreen()
+            LauncherScreen(navController)
+        }
+        composable("ajedrez_app"){
+            AjedrezApp(navController)
         }
     }
 }
